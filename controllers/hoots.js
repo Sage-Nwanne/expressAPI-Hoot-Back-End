0941caad22c9f4150ebf6233c30b4232b77a26de
User Name <user@email.com>
2025-03-17 13:55:59 -0400

    created full crud functionality and comments route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -35,7 +35,10 @@ router.get('/', verifyToken, async (req, res) => {
 // GET '/hoots/:hood id':
 router.get('/:hootId', verifyToken, async (req, res) => {
     try {
-        const hoot = await Hoot.findById(req.params.hootId).populate('author');
+        const hoot = await Hoot.findById(req.params.hootId).populate(
+            'author', 
+            'comments.author');
+            
         res.status(200).json(hoot);
     } catch (error) {
         res.status(500).json({ err: error.message });
@@ -61,14 +64,50 @@ router.put('/:hootId', verifyToken, async (req, res) => {
              {new: true}
             );
 
-        
+        updatedHoot._doc.author = req.user;
         res.status(200).json(updatedHoot);
     } catch (error) {
         res.status(500).json({err: error.message})
     }})
 
 
+    // DELETE '/hoots/:hootId':
+    router.delete('/:hootId', verifyToken, async (req, res) => {
+        try {
+            const hoot = await Hoot.findById(req.params.hootId)
+
+            //check permission: if the user is not the author of the hoot, return error
+            if(!hoot.author.equals(req.user._id)) {
+                return res.status(403).json({err: "You're not allowed to do that!"});
+            }
+            //if it is , delete hoot:
+           const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+            res.status(200).json({msg: "Hoot deleted!"} , deletedHoot);
+        } catch (error) {
+            res.status(500).json({err: error.message})
+        }
+    });
 
 
+    // POST '/hoots/:hootId/comments':
+    router.post('/:hootId/comments', verifyToken, async (req, res) => {
+        try {
+            const hoot = await Hoot.findById(req.params.hootId);
+            req.body.author = req.user._id
+            hoot.comments.push(req.body);
+            await hoot.save();
+
+            // Find the Newly created comment:
+
+            const newComment = hoot.comments[hoot.comments.length - 1]; // latest added comment
+            newComment._doc.author = req.user; 
+            // This line is adding the user information to the newly 
+            // created comment's author field
+
+            res.status(200).json(newComment);
+
+        } catch (error) {
+          res.status(200).json({err: error.message})  
+        }})
 
 module.exports = router;
