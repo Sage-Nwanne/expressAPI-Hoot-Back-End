0b8be7866984309bd022ee082696fcc411e4ee5e
User Name <user@email.com>
2025-03-17 13:27:10 -0400

    complete update functionality

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -32,14 +32,40 @@ router.get('/', verifyToken, async (req, res) => {
         res.status(500).json({ err: error.message });
     }})
 
+// GET '/hoots/:hood id':
+router.get('/:hootId', verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate('author');
+        res.status(200).json(hoot);
+    } catch (error) {
+        res.status(500).json({ err: error.message });
+    }
+
+});
 
 
 
+// PUT '/hoots/:hootId':
+router.put('/:hootId', verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId)
 
+        //check permission: if the user is not the author of the hoot, return error
+        if(!hoot.author.equals(req.user._id)) {
+            return res.status(403).json({err: "You're not allowed to do that!"});
+        }
+        //if it is , update hoot:
+        const updatedHoot = await Hoot.findByIdAndUpdate(
+            req.params.hootId,
+             req.body, 
+             {new: true}
+            );
 
-
-
-
+        
+        res.status(200).json(updatedHoot);
+    } catch (error) {
+        res.status(500).json({err: error.message})
+    }})
 
 
 
